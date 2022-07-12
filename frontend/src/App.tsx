76e987b5b6fb50009539eb43028eb23eaa8ad798
Marcelo Junqueira Teixeira <marcelo.teixeira@softplan.com.br>
2022-07-11 23:53:12 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Ola Mundo</h1>
+  return (
+    <>
+      <h1>Ola Mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
 export default App
+
