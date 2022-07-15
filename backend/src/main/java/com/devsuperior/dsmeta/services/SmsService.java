ac0457a4cf1d6d22900115535b91e6d86368d361
Marcelo Junqueira Teixeira <marcelo.teixeira@softplan.com.br>
2022-07-15 12:40:23 -0300

    Axios, useEffect first request

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -31,7 +31,7 @@ public class SmsService {
 	public void sendSms(Long saleId) {
 		
 		Sale sale = saleRepository.findById(saleId).get();
-		
+							
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
 		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date
