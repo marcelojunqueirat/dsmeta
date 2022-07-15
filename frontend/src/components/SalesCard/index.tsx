81377a0d3489f7734c72bf5fc7ce725a9e4e57b0
Marcelo Junqueira Teixeira <marcelo.teixeira@softplan.com.br>
2022-07-15 14:01:24 -0300

    Send Notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,11 +20,11 @@ function SalesCard() {
 
     // useEffect(()=>{}, []);
     useEffect(() => {
-
+        
         const dmin = minDate.toISOString().slice(0, 10); 
         const dmax = maxDate.toISOString().slice(0, 10); 
-        
-        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)             
             .then(response => {
                 setSales(response.data.content)
             })
@@ -77,7 +77,7 @@ function SalesCard() {
                                     <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={sale.id}/>
                                         </div>
                                     </td>
                                 </tr>
